b96d1fa5c4cf6f0616cb8bfec096c44cb38fbd7b
Dmitry Korotovsky <rmpic30@gmail.com>
2013-05-13 00:47:11 +0400

    cleanup

--- src/main/java/com/korotovsky/client/Main.java
+++ src/main/java/com/korotovsky/client/Main.java
@@ -24,44 +24,5 @@ public class Main {
         });
 
     }
-
-    public void run() {
-        /*System.out.println("Welcome to Client side");
-
-        Socket fromserver = null;
-
-        if (args.length == 0) {
-            System.out.println("use: client hostname");
-            System.exit(-1);
-        }
-
-        System.out.println("Connecting to... " + args[0]);
-
-        fromserver = new Socket(args[0], 10000);
-        BufferedReader in = new
-                BufferedReader(new
-                InputStreamReader(fromserver.getInputStream()));
-        PrintWriter out = new
-                PrintWriter(fromserver.getOutputStream(), true);
-        BufferedReader inu = new
-                BufferedReader(new InputStreamReader(System.in));
-
-        String fuser, fserver;
-
-        while ((fuser = inu.readLine()) != null) {
-            out.println(fuser);
-            fserver = in.readLine();
-            System.out.println(fserver);
-            if (fuser.equalsIgnoreCase("close")) break;
-            if (fuser.equalsIgnoreCase("exit")) break;
-        }
-
-        out.close();
-        in.close();
-        inu.close();
-        fromserver.close();*/
-    }
-
-
 }
 
